634d3f279950392b52963a64025b932bebba80cb
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-01-09 22:54:11 +0600

    remove: remove set

--- Days_21-30/24_expandedForm.js
+++ Days_21-30/24_expandedForm.js
@@ -6,11 +6,11 @@ const expandedForm = (number, expandedNumber = '') => {
     
     // i.e number = 455
     const lengthOfNumber = String(number).length; // 3
-    const numberLength = 1 * Math.pow(10, lengthOfNumber - 1); // 100
+    const numberLength = 1 * Math.pow(10, lengthOfNumber - 1); // 100 formula: 1 * 10^n
     const firstDigit = Math.floor(number / numberLength); // 4
     const expandNumber = firstDigit * numberLength; // 400
 
     return expandedForm(number % numberLength, `${expandedNumber}${expandNumber}+` )
 }
 
-console.log(expandedForm(44500))
+console.log(expandedForm(1015202530))
