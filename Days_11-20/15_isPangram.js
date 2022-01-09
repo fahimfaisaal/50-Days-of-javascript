73c52770e84f85c929b4aed8833b2625cb12eb25
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-01-09 22:37:39 +0600

    feat: update solution

--- Days_11-20/15_isPangram.js
+++ Days_11-20/15_isPangram.js
@@ -1,27 +1,5 @@
-// solution 1
-const isPangram = (input) => {
-    // Code here
-    const alphabets = 'abcdefghijklmnopqrstuvwxyz';
-    let result = true;
-
-    for (let i = 0; i < alphabets.length; i++) {
-        const alphabetRegex = RegExp(alphabets[i], 'i');
-        const isExistAlphabetInInput = alphabetRegex.test(input);
-
-        if (!isExistAlphabetInInput) {
-            result = false;
-            break;
-        }
-    }
-
-    return result;
-}
-
-console.log(isPangram('The quick brown fox jumps over the lazy dog.'))
-
-// solution 2
-const isPangramWithoutLoop = (input) => (
-    new Set(input.toLowerCase().match(/[a-z]/g)).size === 26
+const isPangram = (input) => (
+    input.match(/([a-z])(?!.*\1)/gi).length === 26
 )
 
-console.log(isPangramWithoutLoop('The quick brown fox jumps over the lazy dog.'))
+console.log(isPangram('The qQuick brown fox jumps over the lazy dog.'))
